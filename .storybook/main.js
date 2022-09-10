cf1d01a85e1cab12afbf98af463e8942d8566ec5
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-10 18:43:37 +0530

    use of action and actions addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-viewport"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-viewport", "@storybook/addon-actions"],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -1,27 +1,23 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
+import { action, actions } from "@storybook/addon-actions";
 
 export default {
   title: "Chakra/Button",
   component: Button,
-  argTypes: {
-    onClick: { action: "clicked" },
-  },
 };
 
-const Templete = (args) => <Button {...args} />;
-export const Success = Templete.bind();
-
-Success.args = {
-  colorScheme: "teal",
-  variant: "solid",
-  children: "Success",
+export const Success = () => {
+  return (
+    <Button colorScheme="green" onClick={action("Click handler")}>
+      Success
+    </Button>
+  );
 };
-
-export const Danger = Templete.bind();
-
-Danger.args = {
-  colorScheme: "red",
-  variant: "solid",
-  children: "Danger",
+export const Danger = () => {
+  return (
+    <Button colorScheme="green" {...actions("onClick", "onMouseOver")}>
+      Danger
+    </Button>
+  );
 };

32e6590a83073f521535d3d51c850c2cee37f16d
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-10 08:48:06 +0530

    writing stories

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Button from "./Button";
+
+export default {
+  title: "Button",
+  component: Button,
+};
+
+export const Primary = () => {
+  return <Button variant="primary"> primary </Button>;
+};
+export const Secondary = () => {
+  return <Button variant="secondary"> Secondary </Button>;
+};
+export const Success = () => {
+  return <Button variant="success"> Success </Button>;
+};
+export const Danger = () => {
+  return <Button variant="danger"> Danger </Button>;
+};

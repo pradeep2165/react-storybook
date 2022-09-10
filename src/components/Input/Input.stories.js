0dda592e9b81b0ca87c095d13e5cf356af7a4694
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-10 09:07:11 +0530

    added input storybook

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Input from "./Input";
+
+export default {
+  title: "Title",
+  component: Input,
+};
+
+export const Small = () => {
+  return <Input size="small" placeholder="small text" />;
+};
+export const Medium = () => {
+  return <Input size="medium" placeholder="medium text" />;
+};
+export const Large = () => {
+  return <Input size="large" placeholder="large text" />;
+};
